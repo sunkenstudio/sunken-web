f85d0f3d379fd7551a03ae6fef5b55447bf3e1b8
Dan Warrick <danwarrickdev@gmail.com>
2024-04-13 15:46:30 -0400

    render test for each type

--- src/app/components/ContactForm/__tests__/index.test.tsx
+++ src/app/components/ContactForm/__tests__/index.test.tsx
@@ -5,8 +5,13 @@ import { ContactForm, ContactFormProps } from "..";
 import {
   BrandFixture,
   ContactFixture,
+  DateInputFixture,
+  DropdownInputFixture,
   HeroFixture,
   SectionFixture,
+  TextInputFixture,
+  TextareaInputFixture,
+  TimeInputFixture,
 } from "@/app/fixtures";
 import * as Formik from "formik";
 
@@ -35,5 +40,60 @@ describe("ContactForm", () => {
   it("match snapshot", () => {
     const rendered = renderComponent();
     expect(rendered).toMatchSnapshot();
+  });
+
+  describe("text input", () => {
+    it("should render text input", () => {
+      const rendered = renderComponent({
+        contact: ContactFixture({
+          fields: [TextInputFixture({ label: "Test Label" })],
+        }),
+      });
+      expect(rendered.getByText("TEST LABEL"));
+    });
+  });
+
+  describe("time input", () => {
+    it("should render time input", () => {
+      const rendered = renderComponent({
+        contact: ContactFixture({
+          fields: [TimeInputFixture({ label: "Test Label" })],
+        }),
+      });
+      expect(rendered.getByText("TEST LABEL"));
+    });
+  });
+
+  describe("date input", () => {
+    it("should render date input", () => {
+      const rendered = renderComponent({
+        contact: ContactFixture({
+          fields: [DateInputFixture({ label: "Test Label" })],
+        }),
+      });
+      expect(rendered.getByText("TEST LABEL"));
+    });
+  });
+
+  describe("dropdown input", () => {
+    it("should render dropdown input", () => {
+      const rendered = renderComponent({
+        contact: ContactFixture({
+          fields: [DropdownInputFixture({ label: "Test Label" })],
+        }),
+      });
+      expect(rendered.getByText("TEST LABEL"));
+    });
+  });
+
+  describe("textarea input", () => {
+    it("should render textarea input", () => {
+      const rendered = renderComponent({
+        contact: ContactFixture({
+          fields: [TextareaInputFixture({ label: "Test Label" })],
+        }),
+      });
+      expect(rendered.getByText("TEST LABEL"));
+    });
   });
 });
