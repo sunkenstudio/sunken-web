ed19d74f5dcdcca1c3a8d024fe29a3e2eaa4fdde
Dan Warrick <danwarrickdev@gmail.com>
2024-05-17 10:55:20 -0400

    unused imports

--- src/app/components/Shared/Icon/index.stories.tsx
+++ src/app/components/Shared/Icon/index.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Icon, IconProps, iconMap } from '../Icon';
+import { Icon, IconProps } from '../Icon';
 
 const meta = {
   title: '_Shared/Icon',
