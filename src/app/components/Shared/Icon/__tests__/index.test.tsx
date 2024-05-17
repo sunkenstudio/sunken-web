59fc9cbbcdc805810893b9f8f656c2f5c052feba
Dan Warrick <danwarrickdev@gmail.com>
2024-05-17 11:05:24 -0400

    added tests for positive and busted case

---
+++ src/app/components/Shared/Icon/__tests__/index.test.tsx
@@ -0,0 +1,42 @@
+'use client';
+import '@testing-library/jest-dom';
+import { render, waitFor } from '@testing-library/react';
+
+import { Icon, IconProps } from '..';
+import { testStories } from '@/app/helpers/testStorybook';
+import * as Stories from '../index.stories';
+
+describe('Icon', () => {
+  const renderComponent = (overrides: Partial<IconProps> = {}) => {
+    const defaults = (props: Partial<IconProps>): IconProps => ({
+      type: 'FacebookLogo',
+      size: 32,
+      color: 'black',
+      ...props,
+    });
+    return render(<Icon {...defaults(overrides)} />);
+  };
+
+  describe('All stories should render successfully', () => {
+    testStories(Stories);
+  });
+
+  it('match snapshot', () => {
+    const rendered = renderComponent();
+    expect(rendered).toMatchSnapshot();
+  });
+
+  it('should render InstagramLogo', async () => {
+    const rendered = renderComponent({ type: 'InstagramLogo' });
+    await waitFor(() => {
+      expect(rendered.getByTitle('InstagramLogo icon')).toBeDefined();
+    });
+  });
+
+  it('should render BrokenImage when given an incorrect type', async () => {
+    const rendered = renderComponent({ type: 'foo' });
+    await waitFor(() => {
+      expect(rendered.getByTitle('Image Broken')).toBeDefined();
+    });
+  });
+});
