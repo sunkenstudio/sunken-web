5406c9ca4ffa85df5f64fcbae88e61617b363911
Daniel Warrick <danielwarrick@Daniels-MacBook-Pro.local>
2024-11-08 03:34:16 -0500

    desktop style

--- src/app/components/ProjectSection/index.tsx
+++ src/app/components/ProjectSection/index.tsx
@@ -1,8 +1,9 @@
+'use client';
 import { useBrand } from '@/app/contexts/BrandContext';
 import { StrapiProjectSection } from '@/app/types';
-import { Box, Button, Flex, HStack, Image, Link } from '@chakra-ui/react';
-import React from 'react';
-import { H3, H5, Paragraph } from '../Typography';
+import { Box, Button, HStack, Image, Link, Stack } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { H3, H5 } from '../Typography';
 import { RichText } from '../Shared/RichText';
 import { Icon } from '../Shared/Icon';
 
@@ -11,36 +12,103 @@ export interface ProjectSectionProps {
 }
 
 export const ProjectSection = ({ projectSection }: ProjectSectionProps) => {
-  const { header, bgColor, color, articles } = projectSection;
+  const { header, articles, bgColor, color } = projectSection;
+  console.log({ projectSection });
   const { colors } = useBrand();
+  const [index, setIndex] = useState(0);
+
+  const incrementIndex = () => {
+    if (index + 1 < articles.length) {
+      setIndex(index + 1); // Go to the next slide
+    } else {
+      setIndex(0); // Loop back to the first slide
+    }
+  };
+
+  const decrementIndex = () => {
+    if (index - 1 >= 0) {
+      setIndex(index - 1); // Go to the previous slide
+    } else {
+      setIndex(articles.length - 1); // Loop back to the last slide
+    }
+  };
+
+  const slideWidth = 100 / articles.length; // Calculate dynamic width for each slide
 
   return (
-    <Flex
+    <Stack
       w="100%"
-      color={colors.dark}
-      position="relative"
       minH={'2xl'}
-      justifyContent={'center'}
-      alignItems={'center'}
-      py={{ base: '2rem', md: '' }}
+      bgColor={colors[bgColor]}
+      color={colors[color]}
     >
-      <Box w={{ base: '100%', md: '45%' }}>
-        <H3 mb={'1.25rem'}>{header}</H3>
-        <H5>{articles[0].title}</H5>
-        <RichText content={articles[0].description} />
-        <Link href={articles[0].link} target="_blank">
-          <Icon type="ArrowSquareOut" size={32} color={colors.dark} />
-        </Link>
-        <HStack>
-          <Button>
-            <Icon type="CaretLeft" size={64} color={colors.dark} />
+      <H3 textAlign={'center'} mt={'2rem'}>
+        {header}
+      </H3>
+      <HStack mx="10rem" overflow={'hidden'}>
+        {/* Decrement Button */}
+        {index === 0 ? null : (
+          <Button
+            onClick={decrementIndex}
+            disabled={articles.length <= 1}
+            zIndex={10}
+            bgColor={colors[color]}
+          >
+            <Icon type="CaretLeft" size={32} color={colors[bgColor]} />
           </Button>
-          <Image src={articles[0].images[0].url} />
-          <Button>
-            <Icon type="CaretRight" size={64} color={colors.dark} />
+        )}
+
+        {/* Slider Container */}
+        <Box w="100%">
+          {/* The container that holds all the slides */}
+          <Box
+            display="flex"
+            transition="transform .5s ease"
+            transform={`translateX(-${index * slideWidth}%)`} // Dynamically shift the content by the width of one slide
+            w={`${articles.length * 100}%`} // The container's width is 100% per article, multiplied by the number of articles
+            mr={'1rem'}
+            overflow={'hidden'}
+            mt={'2rem'}
+          >
+            {articles.map((article, i) => (
+              <Box
+                key={i}
+                w="100%" // Each slide takes up 100% of the container width
+                display="flex"
+                opacity={i !== index ? 0 : 1}
+              >
+                <HStack w="100%">
+                  <Image src={article.images[0].url} w="60%" />
+                  <Stack w="40%">
+                    {/* Content of each article */}
+                    <H5>{article.title}</H5>
+                    <RichText content={article.description} />
+                    <Link href={article.link} target="_blank">
+                      <Icon
+                        type="ArrowSquareOut"
+                        size={32}
+                        color={colors[color]}
+                      />
+                    </Link>
+                  </Stack>
+                </HStack>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+
+        {/* Increment Button */}
+        {index === articles.length - 1 ? null : (
+          <Button
+            onClick={incrementIndex}
+            disabled={articles.length <= 1}
+            zIndex={10}
+            bgColor={colors[color]}
+          >
+            <Icon type="CaretRight" size={32} color={colors[bgColor]} />
           </Button>
-        </HStack>
-      </Box>
-    </Flex>
+        )}
+      </HStack>
+    </Stack>
   );
 };
